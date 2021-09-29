bbb7e29ba9cb903284bd65894288eb9ffaa11111
Yegorov Bogdan <eb.you93@gmail.com>
2021-09-29 21:01:12 +0300

    lessons online

---
+++ js/index-1.js
@@ -0,0 +1,38 @@
+// 'use strict'
+
+
+// const a = 0,
+//     b,
+//     c,
+//     d,
+//     e,
+//     f,
+//     g,
+//     h,
+//     i,
+//     j = 0,
+//     k;
+
+
+// const f = 30;
+
+
+// console.log(f);
+
+// -------------------------------------------------------------
+
+// console.log('start');
+
+// const getFirstNumber = prompt('укажите число !');
+// const result = Number(getFirstNumber);
+// console.log(`check number`, Number.isNaN(result));
+// console.log('finish');
+
+// -------------------------------------------------------------
+
+
+
+
+
+
+
---
+++ js/index-2.js
@@ -0,0 +1,31 @@
+// const cart = [54, 28, 105, 70, 92, 17, 120];
+
+// // 2 сделать переменную total до цикла
+// let total = 0;
+
+// // // 1 перебрать массив
+
+// // // for (let i = 0; i < cart.length; i += 1) {
+// // //     console.log(cart[i]);
+
+// // // // 3 акждый элемент приплюсовать к total
+// // //     total += cart[i];
+// // // }
+
+// for (const value of cart) {
+//     total += value; 
+// }
+
+// console.log('Total: ', total);
+
+// // добавить такс
+
+// // for (let i = 0; i < cart.length; i += 1) {
+// //     cart[i] = Math.round(value[i] * 1.1);
+// // }
+
+// // console.log(cart);
+
+// -------------------------------------------------------------
+
+
